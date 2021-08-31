341ef8f4832652b1a844aabecdc3e83d4a051ab0
André Torquato <andretorquato@alu.ufc.br>
2021-08-31 11:55:20 -0300

    chore: create functions to implementations

--- src/modules/users/repositories/implementations/UsersRepository.ts
+++ src/modules/users/repositories/implementations/UsersRepository.ts
@@ -31,15 +31,19 @@ class UsersRepository implements IUsersRepository {
   }
 
   findById(id: string): User | undefined {
-    // Complete aqui
+    const user = this.users.find((user) => user.id === id);
+    return user;
   }
 
   findByEmail(email: string): User | undefined {
-    // Complete aqui
+    const user = this.users.find((user) => user.email === email);
+    return user;
   }
 
   turnAdmin(receivedUser: User): User {
-    // Complete aqui
+    const user = this.users.find((user) => user.id === receivedUser.id);
+    user.admin = true;
+    return user;
   }
 
   list(): User[] {
